0a234f891b92947c1934e5594f7cc6676800c152
Alin Apostol <alin.cristian977@gmail.com>
2021-10-30 00:00:06 +0300

    Added chain of methods susceptible to fail

--- ExceptionsLibrary/DemoCode.cs
+++ ExceptionsLibrary/DemoCode.cs
@@ -2,6 +2,21 @@
 {
     public class DemoCode
     {
+        public int GrandparentMethod(int position)
+        {
+            return ParentMethod(position);
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
         
+        public int GetNumber(int position)
+        {
+            int[] numbers = {1, 4, 7, 2};
+
+            return numbers[position];
+        }
     }
 }
